91b54ec2f4dadaf2dbe630ed5f365a247e6cec20
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-28 17:19:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 4
+let num2 = 15.5
+let num3 = 10
+let num4 = 4
+let multiply = (num1 * num2)
+
+let random = (Math.random() > 0) + 1
+
+let mod = num4 % num3
+
+let max = Math.max(1, 20, 5)
